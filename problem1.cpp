3a95f832aa47c7ec18c947f2271d749b53ec1666
gauravj09 <gsj38@msstate.edu>
2019-07-25 14:51:45 -0500

    Updated Solution to problem 1

---
+++ problem1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+//Definition for singly-linked list.
+
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = NULL;
+        while (head) {
+            ListNode* next = head -> next;
+            head -> next = cur;
+            cur = head;
+            head = next;
+        }
+        return cur;
+    }
+}
