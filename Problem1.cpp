4b5cd04a7ad1daac11e2604db46b7fe934edc1b2
Shazmaan <shazmaan@buffalo.edu>
2019-07-26 13:52:00 -0400

    Updated Optimized sol for Problem1.

--- Problem1.cpp
+++ Problem1.cpp
@@ -22,6 +22,17 @@ struct ListNode {
 
 class Solution {
 public:
+    ListNode* OptimizedreverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr!=NULL){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     ListNode* reverseList(ListNode* head) {
         if(head == NULL){
             return NULL;
