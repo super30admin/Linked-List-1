7abd7edc15d379d5254bc238c55dc6b3540d3c08
chandraS <chandra.shikhar@gmail.com>
2019-07-26 10:02:10 -0500

    Reverse linked list iteratively

--- Sample.java
+++ Sample.java
@@ -5,3 +5,33 @@
 
 
 // Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+    
+           
+        ListNode curr= head;
+        ListNode next= null;
+        ListNode prev = null;
+        
+        while(curr!=null){
+            next= curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr= next;
+        }
+        
+      //the following lines take care of the condition when Linked List is empty
+        head = prev;
+        return head;
+    
+    }
+}
