575e1b77126d987b18a2caad4bf8f89cb062ae6d
bharatreddypothireddy <35764299+bharatreddypothireddy@users.noreply.github.com>
2019-07-26 00:59:08 -0500

    Completed Linked List-1 - Problem 2
    Implemented using length of list and using two pointers sliding window approach

--- Problem2.java
+++ Problem2.java
@@ -3,16 +3,37 @@
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 //
-// Your code here along with comments explaining your approach
-// 1. Calculate length of linked list
-// 2. Start from root node and delete node at the given position from end by pointing deleted node's previous node to deleted nose's next node.
+
 public class removeNthNodeFromEnd {
     private static class ListNode {
         int val;
         ListNode next;
         ListNode(int x) { val = x; }
     }
+    // Your code here along with comments explaining your approach
+    // 1. Use sliding window approach .Have two pointers at start. Move second pointer till n becomes 0 or n times .
+    // 2 . Now slide this window till end i.e move both pointers one step at a time till second pointer reaches end.
+    // 3. Now next node of first pointer will be nth node . Now change next node of first pointer to next next node of first pointer.
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while(n>0 && p2.next!=null){
+            p2 = p2.next;
+            n--;
+        }
+        if(n!=0) return head.next;
+        while(p2.next!=null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        p1.next = p1.next.next;
+        return head;
+    }
+
+    // Your code here along with comments explaining your approach
+    // 1. Calculate length of linked list
+    // 2. Start from root node and delete node at the given position from end by pointing deleted node's previous node to deleted nose's next node.
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
         ListNode copy = head;
         //ListNode result = head;
         int length = 1;
@@ -41,4 +62,5 @@ public class removeNthNodeFromEnd {
         ///System.out.println(length);
         return head;
     }
+
 }
