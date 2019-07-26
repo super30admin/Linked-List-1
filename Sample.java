fbdbc6b5525ad02991b58397dd9c63ab1e79d55e
chandraS <chandra.shikhar@gmail.com>
2019-07-26 14:27:27 -0500

    with one pass

--- Sample.java
+++ Sample.java
@@ -1,7 +1,37 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
-
-// Your code here along with comments explaining your approach
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        int len = 0;
+        
+        ListNode slow= head;
+        ListNode fast= head;
+        
+        while(n>0 && fast!=null){
+            fast=fast.next;
+            n--;
+        }
+      
+      //when only node in the list
+        if(fast==null)
+            return head.next;
+        
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        
+        slow.next=slow.next.next;
+        
+               
+        return head;
+        
+    }
+}
