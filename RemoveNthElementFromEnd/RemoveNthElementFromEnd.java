86383abdec652f3f679f4006b9f6ddd7af2dabe3
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2019-08-22 23:44:55 -0700

    Remove nth element from End

---
+++ RemoveNthElementFromEnd/RemoveNthElementFromEnd.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+ class RemoveNthElementFromEnd {
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+
+		ListNode tempHead = head, tempEnd = head;
+		while(n > 0 && tempEnd.next != null) {
+			tempEnd = tempEnd.next;
+			n--;
+		}
+
+		if(n == 1) return head.next;
+		if(n > 0) return null;
+
+		while(tempEnd.next != null) {
+			tempHead = tempHead.next;
+			tempEnd = tempEnd.next;
+		}
+		tempHead.next = tempHead.next.next;
+
+		return head;
+	}
+}
