bb858ce83c395283f2654625c3a8d34232a858f1
RamyaRaviprakash <rramya@thoughtworks.com>
2019-09-12 16:09:10 -0700

    reverse a singly linked list

--- Sample.java
+++ Sample.java
@@ -1,7 +1,32 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(n)
+// Space Complexity : considering recursion stack, O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :No
 
 
 // Your code here along with comments explaining your approach
+// Approach: 1.Iterate through the list recursively until you find end of list.
+// 2. Initialize head to be the last node. 
+// 3. While coming down the recursion stack, change the nodes to point to previous nodes and return new head.
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null) return null;
+        return reverse(head,null);
+    }
+    private ListNode reverse(ListNode curr, ListNode prev){
+        if(curr==null){
+            return prev;
+        }
+        ListNode head = reverse(curr.next,curr);
+        curr.next=prev;
+        return head;
+    }
+}
