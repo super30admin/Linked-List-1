d0208f5505b3bae1e5515ac2b01f13c566e63cf2
RamyaRaviprakash <rramya@thoughtworks.com>
2019-09-12 16:59:15 -0700

    remove nth node from end of list

--- Sample.java
+++ Sample.java
@@ -30,3 +30,44 @@ class Solution {
         return head;
     }
 }
+
+// Time Complexity :O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :No
+
+
+// Your code here along with comments explaining your approach
+// Approach:
+//1. Keep 2 pointers 'a','b' at a distance of n+1.
+//2. when pointer 'b' reaches end of list, pointer 'a' points to node that has to be deleted. 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int N=n+1;
+        ListNode a = head;
+        ListNode b = head;
+        while(N>0){
+            if(b==null){
+                return head.next;
+            }
+            b=b.next;
+            N--;
+        }
+        while(b!=null){
+            a=a.next;
+            b=b.next;
+        }
+        ListNode t = a.next.next;
+        a.next.next = null;
+        a.next = t;
+        return head;
+    }
+}
