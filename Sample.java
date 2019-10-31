11c27bd655fa651c76dfb571d382e1a26f0dfe11
keerthitejakonuri <keerthi49100@gmail.com>
2019-10-30 23:55:09 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -84,3 +84,31 @@ class Solution2 {
         return head;
     }
 }
+
+
+//Time Complexity :O(N)
+//Space Complexity :O(1)
+//Did this code successfully run on Leetcode :yes
+//Any problem you faced while coding this :Nope
+
+
+//Your code here along with comments explaining your approach
+public class Solution3 {
+    public ListNode detectCycle(ListNode head) {
+        if(null == head || null == head.next){return null;}
+        ListNode slow = head, fast = head;
+        boolean isStart = true;
+        while(isStart || (null != fast && null != fast.next && fast != slow)){
+            if(isStart){isStart = false;}
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(null == fast || null == fast.next){return null;}
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+    return fast;
+    }
+}
