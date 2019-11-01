23d859acad40948114efb375be524dd92c9f7d46
Anoop Martin <33829132+SuperStriker@users.noreply.github.com>
2019-10-31 18:45:50 -0700

    Create Reverse_linked_list.java

---
+++ Reverse_linked_list.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr, prev, succeed;
+        
+        curr = head.next;
+        prev = head;
+        succeed = curr.next;
+        head.next = null;
+        while(succeed!= null){
+            System.out.print("curr "+ curr.val);
+            System.out.print("succeed "+ succeed.val);
+            curr.next = prev;
+            prev = curr;
+            curr = succeed;
+            succeed = succeed.next;
+            System.out.print("curr "+ curr.val);
+            System.out.print("succeed "+ succeed.val);
+            
+            
+            
+        }
+        return curr;
+        
+    }
+}
