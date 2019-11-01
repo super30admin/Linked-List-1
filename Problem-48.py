8a4c953335b6bbfe9cb2bd498a2fb9f6f3e63fb5
sindhu819 <sindu819@gmail.com>
2019-11-01 10:24:55 -0700

    Please Review

--- Problem-48.py
+++ Problem-48.py
@@ -1,7 +1,7 @@
 # Leet code : 206
 # Time complexity -O(N)
 # Space Complexity - O(1) 
-# Approach - So reversing a linked list, simply means reassigning all the next properties, on every node. That means curr.next is reassigned to previous.
+# Approach - So reversing a linked list, simply means reassigning all the next properties, on every node. That means curr.next is reassigned to previous. We can do using stacks.
 
 
 
@@ -28,5 +28,41 @@ class Solution(object):
             prev=curr
             curr=temp
         return prev
-            
+    
+    
+ # Iterative solution  -  Time - O(N),  space-O(1)
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        prev=None
+        curr=head
+        next=curr.next
+        while next!=None:
+            curr.next=prev
+            prev=curr
+            curr=next
+            next=next.next
+        curr.next=prev
+        return curr
+
+# recursive solution - Time O(N) and Space - O(1)
+           
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        ## base case
+        if head==None or head.next==None: return head
+        r=self.reverseList(head.next)
+        head.next.next=head
+        head.next=None
+        return r
+        
             
