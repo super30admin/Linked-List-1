d58ea344722b77850b8350d3958158316bef47a5
Ashwin Saxena <ashwin@Ashwins-MacBook-Air.local>
2019-11-02 21:10:02 -0400

    Completed LinkedList Cycle

---
+++ Problem3.java
@@ -0,0 +1,70 @@
+/**
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+Note: Do not modify the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+
+
+ 
+
+Follow-up:
+Can you solve it without using extra space?
+ */
+
+// Time Complexity : O(n) if list has no cycle and O(n+k) if list has cycle(k-distance for cycle)
+// Space Complexity : O(1) since we are using two nodes: slow and fast
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+//Famous algorithm-Floyds algorithm-LinkedList cycle. It can be seen in circular arrays
+//approach- using two pointers approach- Keep iterating both slow and fast pointers.
+//when slow and fast pointers reaches at an index, we have a cycle
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag) return null;
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
