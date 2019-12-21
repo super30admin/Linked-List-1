5a6f4a5a5bc799d7a74a706b1fc3440fa3ac857b
Prateek Dasgupta <dasguptp@oregonstate.edu>
2019-12-20 21:02:17 -0800

    Adding Problem 1

---
+++ Problem1.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+Leet Code Submitted : YES
+Time Complexity : O(2N)
+Space Complexity : O(N)
+
+Use Stack to store the contents of the list and then pop each element to reverse the list. (Not Optimal)
+
+**/
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        Stack<Integer> s1 =  new Stack<>();
+        
+        if(head == null)
+            return null;
+            
+        ListNode temp = head;
+        while(temp != null){
+            //s1.add(temp.val);
+            temp = temp.next;
+        }
+        
+        temp = head;
+        
+        while(temp != null){
+            temp.val = s1.pop();
+            temp = temp.next;
+        }
+        
+        return head;
+        
+    }
+}
