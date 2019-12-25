14463f1e362f9ae4ba2cf0bc65a9bed4dd93b64a
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-12-25 13:26:29 -0800

    Update Problem_3.py

--- Problem_3.py
+++ Problem_3.py
@@ -27,3 +27,25 @@ class Solution:
             fast = fast.next
         
         return slow
+    
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        fast = self.helper(head)
+        if fast is None:
+            return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+        
+    def helper(self,head):
+        slow = fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return fast
+        return None
