96ffd58363143c12b69f76df1bfc815197edf96d
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2019-12-27 16:04:38 -0800

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -2,7 +2,7 @@
 // Space Complexity - O(1) Since we are using constant space variables for space only
 // This solution worked on LeetCode
 
-// In 
+// In this solution we need to remove the nth node from the end in the list. In order to do that, we need to reach to the node previous to the node that needs to be removed. Which is why we needed a dummy node added at the start of the head. 
 
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
@@ -12,15 +12,15 @@ class Solution {
         ListNode slow = dummy;
         ListNode fast = dummy;
         int count =0;
-        while(count < n && fast.next != null){
+        while(count < n && fast.next != null){  // traverse the fast pointer n times to reach the nth element from the start
             fast = fast.next;
             count+=1;
         }
         while( fast.next != null){
-            fast = fast.next;
+            fast = fast.next;               // traverse the slow starting from the dummy and the fast starting from the nth node till the fast reaches the last node. Doing so, the slow will reach the n-1th node from the end
             slow = slow.next;
         }
-        slow.next = slow.next.next;
-        return dummy.next;        
+        slow.next = slow.next.next;         // Now, point the n-1th node next to the n+1 node from the end. This will skip the nth node from the end
+        return dummy.next;                  // Return the head or dummy.next
     }
 }
