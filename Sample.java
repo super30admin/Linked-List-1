33ba117c3a927b792e12b51ab1ff902a1a373664
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-02-27 20:26:09 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,43 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+Problem1: Reverse a LinkedList
+
+Approach 1: 
+Iterative: have 3 pointers as prev, current & next and change their references until we hit the null
+Time Complexity : O(n)
+Space Complexity : O(1)
+Did this code successfully run on Leetcode : Yes
+  
+Approach 2  
+Recursive: 
+
+Time Complexity : O(n)
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : Yes
+
+
+
+Problem 2: LinkedList Cycle II
+
+Approach 1: Using HashSet
+Time Complexity : O(n)
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : Yes
+
+Approach 2: Using two pointers, once we detect the list, reset either one of the pointers and 
+if they meet its the start of the linked list.
+Time Complexity : O(n)
+Space Complexity : O(1)
+Did this code successfully run on Leetcode : Yes  
+
+
+
+Problem 3: Remove Nth Node From End of List
+
+
+
+
+
