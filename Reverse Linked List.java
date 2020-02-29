67db97221686d8fe8c44f3777405094414138949
Pavani <pavani.somarouthu@sjsu.edu>
2020-02-28 17:40:13 -0800

    linkedlist

--- Reverse Linked List.java
+++ Reverse Linked List.java
@@ -5,16 +5,50 @@
 
 
 // Your code here along with comments explaining your approach
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
-    ListNode prev = null;
-    ListNode curr = head;
-    while (curr != null) {
-        ListNode nextTemp = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = nextTemp;
-    }
-    return prev;
+     
+        if(head==null || head.next ==null){return head;}
+        
+        ListNode curr= reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return curr;
     }
 }
+
+
+//ITERATIVE SOLUTION
+    //     if(head ==null || head.next ==null){return head;}
+    //  ListNode prev=null;
+    //  ListNode curr=head;
+    //      ListNode next=curr.next;
+    //     while(next!=null){
+    //         next=curr.next;
+    //         curr.next=prev;
+    //         prev=curr;
+    //         curr= next;            
+    //     }
+    //     return prev;
+    // }
+
+//SOLUTION 2
+ // public ListNode reverseList(ListNode head) {
+ //    ListNode prev = null;
+ //    ListNode curr = head;
+ //    while (curr != null) {
+ //        ListNode nextTemp = curr.next;
+ //        curr.next = prev;
+ //        prev = curr;
+ //        curr = nextTemp;
+ //    }
+ //    return prev;
+ //    }
