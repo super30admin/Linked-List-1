46f0927eb6d5daa11654a28a6cb2cd70c07db2f8
arcsoftech <arihant_Chhajed@yahoo.in>
2020-04-09 18:28:34 -0500

    Completed two problem

---
+++ ReverseList.java
@@ -0,0 +1,25 @@
+// Time Complexity : O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev,dummy;
+        dummy = null;
+        prev = null;
+        
+        while(head != null)
+        {
+            
+            dummy = head;
+            head = head.next;
+            dummy.next = prev;
+            prev = dummy;
+        }
+        return dummy;
+        
+    }
+}
