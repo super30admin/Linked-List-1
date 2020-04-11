833301725dc9b774bd3669887b7bdc0af4a48845
Karthik Bangera <adikar08@gmail.com>
2020-04-10 20:46:29 -0400

    Added solution to remove nth node

--- Remove_Nth_Node.java
+++ Remove_Nth_Node.java
@@ -1,7 +1,32 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Failed for [1],1
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+       
+        ListNode slow = head;
+        ListNode fast = head;
+        int len=0;
+        
+        for(int i=0;i<n;i++){
+            if(fast==null)return null;
+            fast=fast.next;
+            len++;
+        }
+        
+        
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return head;
+    }
+}
