51bc10c935c9656e6804c169e3b3837e3afdc8a5
Siyami Avci <siyamiavci@gmail.com>
2020-05-14 16:40:48 -0700

    add solution 3

--- p3.js
+++ p3.js
@@ -1,5 +1,17 @@
 // ## Problem3 (https://leetcode.com/problems/linked-list-cycle-ii/)
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+
+var detectCycle = function(head) {
+    let set = new Set();
+    while(head !== null) {
+        if(set.has(head)) {
+            return head;
+        }
+        set.add(head);
+        head = head.next;
+    }
+    return null;
+};
