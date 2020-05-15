1a2dbd82d5cdde3e98eeb0b2a96571eefc4e8855
Siyami Avci <siyamiavci@gmail.com>
2020-05-14 18:09:12 -0700

    add solution 2

--- p2.js
+++ p2.js
@@ -1,5 +1,35 @@
 // ## Problem2 (https://leetcode.com/problems/remove-nth-node-from-end-of-list/)
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+const reverse = function(head) {
+    if(!head || !head.next) return head;
+    let prev = head, cur = head.next, next = cur.next;
+    prev.next = null;
+    while(cur.next) {
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+        next = next.next;
+    }
+    cur.next = prev;
+    return cur;
+}
+
+const removeNthFromEnd = function(head, n) {
+    head = reverse(head);
+    // Remove Nth Node From End of List
+    let cur = head.next, prev = head, count = 2;
+    if(n === 1) head = cur;
+    else {
+        while(count !== n) {
+            prev = cur;
+            cur = cur.next;
+            count++;
+        }
+        prev.next = cur.next;
+    }
+    return reverse(head);
+};
