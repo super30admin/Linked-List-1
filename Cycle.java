631906c8fa80b8c4085223150754e5621a9148a1
unknown <harale.s@husky.neu.edu>
2020-06-18 10:51:34 -0400

    Completed LinkedList cycle

---
+++ Cycle.java
@@ -0,0 +1,34 @@
+Time complexity: O(N)
+Space:O(1)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {             
+        ListNode fast = head, slow = head;       
+        boolean flag = false;        
+        while(fast != null && fast.next != null){            
+            slow = slow.next;      
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag) return null;
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
