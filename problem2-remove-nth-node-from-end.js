0d8c93bfe183e53125bb4ad2357a10f74cdf52c9
Ken Ore <v-kore@expediagroup.com>
2020-09-10 22:42:55 -0500

    added comments to problem2

--- problem2-remove-nth-node-from-end.js
+++ problem2-remove-nth-node-from-end.js
@@ -1,10 +1,19 @@
 //https://leetcode.com/problems/remove-nth-node-from-end-of-list/
-//// Time Complexity :
-//// Space Complexity :
-//// Did this code successfully run on Leetcode :
+//// Time Complexity : O(n)
+//// Space Complexity : O(1) space for a dummy (sentinel), p1, p2
+//// Did this code successfully run on Leetcode : yes
 //// Any problem you faced while coding this :
 
+// off by 1 errors, special casing, just start p1,p2 at sentinel
+
 //// Your code here along with comments explaining your approach
+
+// create a sentinel / dummy node whose next points to head
+// (sentinel dummy will handle removing 1 element from list of size 1)
+// set p1, p2 to point to sentinel
+// advance p2 n times
+// now advance p1 and p2 each one time until p2.next == null
+// p1 is now at one before the node to delete, set p1.next = p1.next.next to remove the node
 var ListNode, list, list1, removeNthFromEnd;
 
 ListNode = function(val, next = null) {
