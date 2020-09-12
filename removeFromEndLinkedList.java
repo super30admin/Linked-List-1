17cb196de71a6e6fd43653cc5d415bea3d758383
pritesh-chauhan <54586794+pritesh-chauhan@users.noreply.github.com>
2020-09-12 13:18:23 -0400

    Update removeFromEndLinkedList.java
    Updated Space complexity

--- removeFromEndLinkedList.java
+++ removeFromEndLinkedList.java
@@ -1,5 +1,5 @@
 //Time Complexity: O(n) 
-//Space complexity: O(2n)
+//Space complexity: O(1)
 
 public class removeFromEndLinkedList extends reverseLinkedList{
     public ListNode removeNthFromEnd(ListNode head, int n) {
