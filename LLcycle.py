31ce356181830ed69d724e0941c65f0a8a6158c3
Charmi Patel <36853058+Charmi44@users.noreply.github.com>
2020-09-16 01:22:49 -0400

    Linkedlist2-please-review

---
+++ LLcycle.py
@@ -0,0 +1,45 @@
+"""
+142. Linked List Cycle II
+
+time = 
+space = 
+
+Successfully excecuted on leetcode
+
+
+Approach --
+1. Save the current node's next in to temp
+2. Save previous node as curr node's next
+3. Now set previous node as current and temp as current
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Cycle:
+        
+        #1. Recursion : time - O(n), space = O(n); where n = num of nodes
+        if not head or not head.next:
+            return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
+                
+        
+        #2. Iterative approach, time = O(n), space = O(1); where n = num of nodes
+        prev = None
+        curr = head
+        while curr:
+            nextTemp = curr.next
+            print("nextTemp :", nextTemp)
+            curr.next = prev
+            print("curr next :", curr.next)
+            prev = curr
+            print("prev : ", prev)
+            curr = nextTemp
+            print("curr : ", curr)
+        print("PREV HERE : ", prev)
+        return prev
