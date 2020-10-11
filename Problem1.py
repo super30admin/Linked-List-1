ee78ca23d8f4b69f46caa32a0a360378d429cec6
Yogesh Kumar Singh <20600483+yogesh696ksingh@users.noreply.github.com>
2020-10-11 10:57:40 -0700

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,27 @@
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : 
+# Your code here along with comments explaining your approach
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # prev = None
+        # curr = head
+        # while head:
+        #     curr = head
+        #     head = head.next
+        #     curr.next = prev
+        #     prev = curr
+        # return prev
+        if head is None or head.next is None:
+            return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
