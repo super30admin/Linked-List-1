bec298ccb1b0f2c01f6e56592df0778fd659f577
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-10 19:19:55 -0800

    Iterative and Reverse Approach

--- ReverseLinkedList206.java
+++ ReverseLinkedList206.java
@@ -1,7 +1,7 @@
 package dec9th2020;
 
 public class ReverseLinkedList206 {
-
+// Recursive Approach
 	public ListNode reverseList(ListNode head) {
 
 		if (head == null || head.next == null)
@@ -12,6 +12,27 @@ public class ReverseLinkedList206 {
 		return reversed;
 
 	}
+	
+	// Iterative approach
+	public ListNode reverseList(ListNode head) {
+
+		if (head == null || head.next == null)
+			return head;
+		
+		Stack<ListNode> s = new Stack<>();
+		while(head.next != null){
+			s.push(head);
+			head = head.next;
+		}
+		ListNode reversed = head;
+		while(!s.isEmpty()){
+			head = s.pop();
+			head.next.next = head;
+			head.next = null;
+		}
+		return reversed;
+	}
+	
 
 	public class ListNode {
 		int val;
