ccf343ea981f148d459efffbb38dad5fde622b0d
rajeshoo7 <rajeshsai.kammari@gmail.com>
2020-12-15 18:16:59 -0600

    completed LinkedList-1

---
+++ LinkedListCycle|I.java
@@ -0,0 +1,49 @@
+// TC: O(N)
+// SC: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head == null)
+            return null;
+        
+        ListNode slow = head, fast = head;
+        boolean hasCycle = false;
+        
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                hasCycle = true;
+                break;
+            }
+        }
+        
+        
+        if(!hasCycle){
+            return null;
+        }
+        
+        
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+        
+    }
+}
