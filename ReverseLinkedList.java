b554d0cd61846d6d2267637c040923c39aed8d3f
nieldeokar <nilesh@upperlevelcrm.com>
2021-01-15 12:33:47 +0530

    reverse using recursion

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -17,36 +17,84 @@ public class ReverseLinkedList {
  }
 
   public static void main(String[] args){
-
     ListNode head = new ListNode(1);
     head.next = new ListNode(2);
     head.next.next = new ListNode(3);
     head.next.next.next = new ListNode(4);
     head.next.next.next.next = new ListNode(5);
-
+    System.out.println("\n========= Recursive start ========");
     ListNode temp = head;
+
     while(temp != null){
       System.out.print(temp.val + "->");
       temp = temp.next;
     }
-
-    ListNode prev = null;
-    ListNode curr = head;
-
-    while (curr != null) {
-      ListNode nextTemp = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = nextTemp;
-    }
+    ListNode prev = reverseRecursive(head);
 
     temp = prev;
     System.out.print("\n");
+
     while(temp != null){
       System.out.print(temp.val + "->");
       temp = temp.next;
     }
 
-    }
 
+    head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
+
+    reverseIterative(head);
+
+  }
+
+  // Time Complexity : O(n)
+  // Space Complexity : O(n) - recursive space
+  // Did this code successfully run on Leetcode : yes
+  // Any problem you faced while coding this : no
+  public static ListNode reverseRecursive(ListNode head){
+    if(head == null || head.next == null) return head;
+
+    ListNode reversed = reverseRecursive(head.next);
+    head.next.next = head;
+    head.next = null;
+
+    return reversed;
+  }
+
+
+
+    // Time Complexity : O(n)
+    // Space Complexity : O(1)
+    // Did this code successfully run on Leetcode : yes
+    // Any problem you faced while coding this : no
+    public static void reverseIterative(ListNode head){
+
+      System.out.println("\n========= iterative start ========");
+      ListNode temp = head;
+      while(temp != null){
+        System.out.print(temp.val + "->");
+        temp = temp.next;
+      }
+
+      ListNode prev = null;
+      ListNode curr = head;
+
+      while (curr != null) {
+        ListNode nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+      }
+
+      temp = prev;
+      System.out.print("\n");
+      while(temp != null){
+        System.out.print(temp.val + "->");
+        temp = temp.next;
+      }
+      System.out.print("\n");
+    }
   }
