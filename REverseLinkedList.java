7cd84f2bf9c61230b1352f6f446b52f10dc039d0
abhisheksharma0904 <76174342+abhisheksharma0904@users.noreply.github.com>
2021-02-11 23:43:03 +0530

    Create REverseLinkedList.java

---
+++ REverseLinkedList.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+class Solution 
+{
+
+    public ListNode reverseList(ListNode head) 
+    {
+        // return reverseWithStack(head);
+
+        return reverseInplace(head);
+    }
+
+    private ListNode reverseWithStack(ListNode node)
+    {
+        Stack<Integer> stack = new Stack<>();
+        ListNode dummy = new ListNode(0);
+
+        while(node!=null)
+        {
+            stack.push( node.val);
+            node = node.next;
+        }
+
+        ListNode temp = dummy;
+
+        while( !stack.isEmpty() )
+        {
+            int currentValue = stack.pop();
+
+            ListNode newnode = new ListNode(currentValue);
+
+            temp.next = newnode;
+            temp = temp.next;
+        }
+
+
+        return dummy.next;
+    }
+
+    private ListNode reverseInplace(ListNode head)
+    {
+        if(head == null) return head;
+
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = current.next;
+
+        while(next != null)
+        {
+            current.next = previous;
+            previous = current;
+            current = next;
+            next = next.next;
+
+        }
+
+        current.next = previous;
+
+        return current;
+    }
+}
