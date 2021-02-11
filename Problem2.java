a65022ae5b836e458335382f410b800dea6f9be3
Kapish Garg <kapish5467@gmail.com>
2021-02-11 10:02:37 -0800

    completed

--- Problem2.java
+++ Problem2.java
@@ -1,9 +1,7 @@
 // Time Complexity :O(n)
 // Space Complexity :O(1)
 // Did this code successfully run on Leetcode :Yes
-// Any problem you faced while coding this :Yes
-//I tried the approach of finding the size of the list but could not figure out one pass solution.
-
+// Any problem you faced while coding this :No
 
 // Your code here along with comments explaining your approach
 
@@ -19,30 +17,43 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head==null || head.next==null)
-            return null;
-        int size=1;
-        ListNode slow=head;
-        while(slow.next!=null)
+       /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy= new ListNode(-1);//dummy node in case of n> size of the list
+        dummy.next=head;
+        
+        ListNode slow= dummy;
+        ListNode fast=dummy;
+        
+        for(int i=0;i<n;i++)
         {
-            slow=slow.next;
-             size++;
+          fast=fast.next;  //fast pointer will be n steps ahead
         }
-        int i=0;       
-        ListNode temp=head;
-        while(i<(size-n) && temp.next!=null)
+        
+        while(fast.next!=null)
         {
-            if(i==(size-n)-1)
-            {
-                ListNode targ=temp.next;
-                temp.next=temp.next.next;
-                targ.next=null;
-            }
-            temp=temp.next;
-            i++;
-                
+            fast=fast.next;
+            slow=slow.next;//slow will reach at n+1 from the end.
         }
-        return head;
+        ListNode temp=slow.next;
+        slow.next=slow.next.next;
+        temp.next=null;
+        
+        return dummy.next;
+        
+        
+    }
+}
         
     }
 }
