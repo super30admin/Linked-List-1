b94b3bbda6be86e7e8587464e9a54142ec347f84
Nirmi <nirmalasnawale@gmail.com>
2021-02-14 19:00:55 -0600

    Linked List 1 completed; Please review

---
+++ ReverseList_LC206.java
@@ -0,0 +1,31 @@
+/**
+
+Time: O(n) - visiting every node
+Space: O(1) - constant
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null)
+            return null;
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
