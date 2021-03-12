1e4fe4102225a83b53c5d23c3379a475aa4cd53f
sharan <shrnssh2510@gmail.com>
2021-03-11 23:05:20 -0500

    finished exercises

---
+++ cycle.cpp
@@ -0,0 +1,43 @@
+//TC: O(n) where n is the number of nodes
+//SC: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        //check if there is a cycle:
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *entry = head;
+        
+        while(slow !=NULL && fast != NULL && fast->next != NULL){
+            
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){
+                
+                while(slow != entry){
+                    slow = slow->next;
+                    entry = entry->next;
+                }
+                
+                return entry;
+                
+            }
+                
+        }
+        
+        return NULL;
+        
+    }
+};
