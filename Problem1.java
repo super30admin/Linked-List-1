07e69e71b5f787001dda2249fa387d489df4f1e4
himali-s <himali.shelat@utdallas.edu>
2021-04-11 00:17:28 -0500

    solved qs3

--- Problem1.java
+++ Problem1.java
@@ -1,4 +1,16 @@
 //reverse linked list 
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+
+// Your code here along with comments explaining your approach
+/*
+It is three pointer approach. First pointer will point to head, another to head.next and another to head's
+prev node. while we reach the end of the node, we reverse each node. We make curr's next to prev node and then 
+moving onto next of curr
+*/
 class Problem1{
     public static void main(String[] args){
         ListNode head = new ListNode(1);
