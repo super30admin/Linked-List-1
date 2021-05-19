74040210eb82f67e8981339f0be8e4f795a57801
Avinash-5120 <44517616+Avinash-5120@users.noreply.github.com>
2021-05-19 13:52:46 -0400

    Linked-List-1 completed

--- Sample.java
+++ Sample.java
@@ -1,7 +1,94 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
 
 
-// Your code here along with comments explaining your approach
+// Problem1 (https://leetcode.com/problems/reverse-linked-list/)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode last=head.next;
+        while(last!=null)
+        {
+            curr.next=prev;
+            prev=curr;
+            curr=last;
+            last=last.next;
+        }
+        curr.next=prev;
+        return curr;
+        
+    }
+}
+
+
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+
+//Problem2 (https://leetcode.com/problems/remove-nth-node-from-end-of-list/)
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode slow=dummy;
+        ListNode fast=dummy;
+        int count=0;
+        while(count < n)
+        {
+            count++;
+            fast=fast.next;
+        }
+        while(fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return dummy.next;
+    }
+}
+
+
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+
+//Problem3 (https://leetcode.com/problems/linked-list-cycle-ii/)
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        boolean flag=false;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+            {
+                flag=true;
+                break;
+            }
+        }
+        if(!flag)
+            return null;
+        fast=head;
+        while(slow!=fast)
+        {
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+        
+    }
+}
+
