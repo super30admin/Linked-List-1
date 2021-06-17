7e70f108c7b9b314428006e337a2558436499baf
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-06-17 16:31:41 +0530

    Completed Problem 2 and 3

--- Problem3.py
+++ Problem3.py
@@ -1,4 +1,27 @@
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#used map to keep track
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if head == None:
+            return None
+        a = {}
+        node = head
+        while node !=None:
+            if node not in a:
+                a[node] = True
+            else:
+                return node
+            node = node.next
+        return None
+            
+            
