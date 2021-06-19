7713a32e24e6775bf163743f3613b959857e8131
vigneshpurushothaman97 <vigneshpurushothaman97@gmail.com>
2021-06-19 17:25:42 +0530

    Completed LinkedList-1

---
+++ RemothNthNodeFromEnd.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        int count = 0;
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while(count<=n && fast!=null)
+        {
+            fast = fast.next;
+            count++;
+        }
+        while(fast!=null)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
+
+
+//Time Complexity is O(n)
+//Space Complexity is O(1)
