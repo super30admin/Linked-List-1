a451cb357fa649921c4a4fa485319d7d9377ea10
Kranthi Nuthalapati <54932650+nkranthi28@users.noreply.github.com>
2021-06-19 10:52:46 -0700

    Completed Sprint Linked-List-1

--- reverse-linkedlist.py
+++ reverse-linkedlist.py
@@ -21,20 +21,44 @@ class Solution(object):
         #     new_head=new_head.next
         # return temp
 
-        #Iterative
-#         prev=None
-#         curr=head
-#         next=None
+#Iterative
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Base condition if list is empty
+        if head is None: return
         
-#         while curr:
-#             next=curr.next
-#             curr.next=prev
-#             prev=curr
-#             curr=next
+        # Consider three pointers: Prev, curr and fast
+        prev = None
+        curr = head
+        fast = head.next
+        
+        # Until fast becomes null, keep iterating
+        # Make the curr.next point to previous
+        # Change the previous pointer to the current
+        # Change the current pointer to the fast
+        # Change the fast pointer to fast.next
+        while fast:
+            curr.next = prev
+            prev = curr
+            curr = fast
+            fast = fast.next
+        
+        # At the end, everything will be reversed except the last one
+        # This is happening because the while condition executed till fast is null and not till current is null. 
+        # So current will be at the last node when fast becomes null 
+        # This is to make that last node in current point to the previous
+        curr.next = prev
+        # Return current pointer since the linked list has reversed
+        return curr
             
-#             head=prev
             
-#         return head
 
         #Recursive
         if head is None or head.next is None:
