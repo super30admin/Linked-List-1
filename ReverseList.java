e6c6074b31241691551aba2bdd88aff17d21ed42
haritha938 <haritha.nimmagadda@student.csulb.edu>
2021-06-22 17:49:17 -0700

    Submitting Linked-List-1 solutions

---
+++ ReverseList.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// LeetCode: 206. Reverse Linked List -Easy
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next=null;
+        
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
