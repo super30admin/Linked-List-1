56d9d41a11737913dc01e36794899d91b69ace4b
Arjun Hegde <arjunhegde372@gmail.com>
2021-06-25 18:49:29 -0500

    Create LinkedListCycle.java

---
+++ LinkedListCycle.java
@@ -0,0 +1,26 @@
+
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+
+
+        public class Solution {
+            public ListNode detectCycle(ListNode head) {
+                ListNode slow = head;
+                ListNode fast = head;
+        
+                while (fast!=null && fast.next!=null){
+                    fast = fast.next.next;
+                    slow = slow.next;
+                    
+                    if (fast == slow){
+                        ListNode slow2 = head; 
+                        while (slow2 != slow){
+                            slow = slow.next;
+                            slow2 = slow2.next;
+                        }
+                        return slow;
+                    }
+                }
+                return null;
+            }
+        }
