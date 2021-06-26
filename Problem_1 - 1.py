7e32d0d5d8aa79c40f500cfe509af9f05318bc93
AdithyaHolla <56132230+AdithyaHolla@users.noreply.github.com>
2021-06-25 19:02:24 -0700

    Update Problem_1 - 1.py

--- Problem_1 - 1.py
+++ Problem_1 - 1.py
@@ -1,31 +1,26 @@
 """
-Brute Force
 Time Complexity : O(n)
-Space Complexity : O(n)
+Space Complexity : O(1)
     
 """
+
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
+    def reverseList(self, head: ListNode) -> ListNode:
         if not head:
-            return None 
+            return head 
         
-        set_ = set()
+        prev = None 
+        cur = head 
         
-        while head:
-            if head in set_:
-                return head
-            else:
-                set_.add(head)
-            head = head.next
-                
-        return None 
-            
-            
+        while cur:
+            temp = cur.next 
+            cur.next = prev 
+            prev = cur 
+            cur = temp 
         
-        
+        return prev 
