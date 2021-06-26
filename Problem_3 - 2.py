0158a09d103b7bfaa049146feeeb97c26a0a7b3b
AdithyaHolla <56132230+AdithyaHolla@users.noreply.github.com>
2021-06-25 19:02:32 -0700

    Create Problem_3 - 2.py

---
+++ Problem_3 - 2.py
@@ -0,0 +1,40 @@
+"""
+Time Complexity : O(n)
+Space Complexity : O(1)
+    
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if not head:
+            return None 
+        
+        slow = fast = head 
+        flag = False 
+        
+        while fast != None and fast.next != None:
+            slow = slow.next 
+            fast = fast.next.next 
+            if slow == fast:
+                flag = True 
+                break 
+        
+        if not flag:
+            return None 
+        
+        slow = head 
+        while fast != slow:
+            slow = slow.next 
+            fast = fast.next 
+        
+        return fast
+            
+            
+        
+        
