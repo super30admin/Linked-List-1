f7fadc9b61955c1eaf1603bd2d1beecfefeb1509
Radhika Kulkarni <radhikankulkarni@gmail.com>
2021-07-13 08:44:59 +0530

    Completed reverse linked list using recursion

---
+++ ReverseLinkedListRecursion.java
@@ -0,0 +1,13 @@
+// TC: O(n)
+// SC: O(n)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode reversed = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reversed;
+    }
+}
