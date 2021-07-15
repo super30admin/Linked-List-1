529732630bb971908579b34ce3e75c3585b99f06
jisakp7 <kavya.pathi@jbhunt.com>
2021-07-15 17:25:23 -0500

    RemoveNthNodeFromEndofList 1-pass solution

--- java/RemoveNthNodeFromEndofList.java
+++ java/RemoveNthNodeFromEndofList.java
@@ -38,3 +38,45 @@ class Solution {
         return dummy.next;
     }
 }
+
+1-Pass Solution-----------------------------------------------------------------------------
+/*
+The Fast pointer advances the list by n+1n+1 steps from the beginning, while the slow pointer starts from the beginning of the list.
+Now, both pointers are exactly separated by nn nodes apart.
+We maintain this constant gap by advancing both pointers together until the fast pointer arrives past the last node.
+We link the slow's nex pointer to next next so that link to the nth node from last is removed.
+ */
+/*
+consider fast and slow pointers where fast will traverse until the
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        int count = 1;
+        while(count<=n+1){
+            count++;
+            fast = fast.next;
+        }
+        System.out.println(count);
+
+        while(fast!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
