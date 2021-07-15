4d09a72a1d0e8062ff36959db2bdad327ba60daa
Manasa Geemarahalli Prakash <manasamanu.gp@gmail.com>
2021-07-15 12:55:03 -0400

    linked-list-1

---
+++ reverseLinkedList.java
@@ -0,0 +1,33 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+
+// Your code here along with comments explaining your approach
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ //iterative approach
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+    ListNode prev = null;
+    ListNode curr = head;
+    while (curr != null) {
+        ListNode nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+    }
+}
