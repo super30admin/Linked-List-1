558aff01df91de5992db0c1b477f780599c7797b
Deshpande <mdeshpande@newyorklife.com>
2021-07-22 20:13:37 -0400

    Linked List 1 completed

---
+++ removeNthNode.java
@@ -0,0 +1,38 @@
+//TC: O(N)
+//SC: O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null)
+            return head;
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+        
+        int counter = 0;
+        
+        while(counter <= n) {
+            fast = fast.next;
+            counter++;
+        }
+        
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return dummy.next;
+    }
+}
