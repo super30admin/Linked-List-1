28487ecaa4989ec9fc37f442202169a5e7ba98e5
Sai Sruthi Mareedu <smareed1@sundevils.asu.edu>
2021-08-30 15:46:20 -0700

    completed

---
+++ removen.java
@@ -0,0 +1,35 @@
+// Time Complexity : o(n)
+// Space Complexity :o(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : no
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode first = dummy;
+    ListNode second = dummy;
+    for (int i = 1; i <= n + 1; i++) {
+        first = first.next;
+    }
+    while (first != null) {
+        first = first.next;
+        second = second.next;
+    }
+    second.next = second.next.next;
+    return dummy.next;
+}
+}
