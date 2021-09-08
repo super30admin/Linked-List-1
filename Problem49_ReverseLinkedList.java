8be8f5ed5a80fe0e137099fd5161a8efebd046cc
balasudip <balasudip6@gmail.com>
2021-09-07 18:19:09 -0700

    Completed Sprint Linked-List-1

---
+++ Problem49_ReverseLinkedList.java
@@ -0,0 +1,20 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
