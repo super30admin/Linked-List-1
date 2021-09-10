4bc7baab54396d91da57151d377d2f01e174e080
sandhya <goswami.sa@northeastern.edu>
2021-09-10 00:16:33 -0400

    Completed DetectCycleInLinkedList RemoveNthFromEnd RevereLinkedList

---
+++ RemoveNthFromEnd.java
@@ -0,0 +1,43 @@
+// Time Complexity : O(n) n is length of linkedlist
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+1. take two pointers first and second
+2. move first by one till count < n
+3. now move both points by one untill first point reaches at end 
+4. in this way your second pointer will be just a index ahead the Nth element(deleted) 
+5. just set the next of this element to next.next
+
+class Solution {
+    // one pass solution
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+       
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        
+        if(head == null)
+            return dummy.next;
+        
+        int count =0;
+        while(count<=n)
+        {
+            first = first.next;
+            count++;
+        }
+        
+        while(first != null)
+        {
+           first = first.next;
+            second = second.next;
+        }
+        
+        second.next = second.next.next;
+        
+        return dummy.next;
+    }
+}
---
+++ ReverseLinkedList.java
@@ -0,0 +1,32 @@
+// Time Complexity : O(n) length of list
+// Space Complexity :O91)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+// recursive approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode ele = reverseList(head.next);
+        
+        head.next.next = head;
+        head.next = null;
+        
+        return ele;
+    }
+}
