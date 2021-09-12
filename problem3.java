3c002a0e01da30f8e751c0915051f6d06b04f034
shivamagarwal2996 <39870711+shivamagarwal2996@users.noreply.github.com>
2021-09-12 01:59:05 -0400

    3 problems done

---
+++ problem3.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head == null ) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast)
+            {   
+                flag = true;
+                break;
+            }
+        }
+        if (flag == false) return null;
+        fast = head;
+        while(slow != fast)
+        {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+        
+    }
+}
