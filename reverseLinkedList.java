1dbe06cbcfaaed70a93d6099451855612974d411
AkashDevendranath <akashdev248@gmail.com>
2021-09-13 20:56:12 -0500

    LinkedList-1(Java). Please Review

---
+++ reverseLinkedList.java
@@ -0,0 +1,28 @@
+// Time Complexity : O(n) 
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+public class reverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fast = head.next;
+        while(fast != null){
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;
+        }
+        curr.next = prev;
+        return curr;
+    }
+}
