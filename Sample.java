061e8ad1268ec838894f1e924aa21f5b4f5af685
Gaurav Jaisinghani <47674146+gauravha@users.noreply.github.com>
2021-09-14 16:07:57 -0400

    linkedlIst - 1 completed

--- Sample.java
+++ Sample.java
@@ -5,3 +5,95 @@
 
 
 // Your code here along with comments explaining your approach
+// o(n) time and constant space
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null ||head.next == null) return head;
+        //logic
+        ListNode reversed = reverseList(head.next);
+        System.out.println(head.val);
+        head.next.next= head;
+        head.next = null;
+        return reversed;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow= dummy;
+        ListNode fast = dummy;
+        //create the gap
+        int count =0;
+        while(count <=n){
+            fast = fast.next;
+            count++;
+        }
+        while(fast!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        while(fast!=null && fast.next != null ){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
+
+        }
+        if(flag!= true){
+            return null;
+        }
+        fast = head;
+        while(slow!=fast){
+            slow = slow.next;
+            fast =fast.next;
+        }
+        return slow;
+
+    }
+}
