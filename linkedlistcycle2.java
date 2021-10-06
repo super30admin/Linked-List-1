8fba76d72da192039a9f442152d442da5b9b5cd7
hackerrank <me@hackerrank.com>
2021-10-05 19:23:06 -0700

    Completed Sprint Linked-List-1

---
+++ linkedlistcycle2.java
@@ -0,0 +1,24 @@
+time complexity: O(n)
+space complexity: O(1)
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+       ListNode slow = head;
+       ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                ListNode slow2 = head;
+                while (slow2 != slow) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        return null; 
+    }
+}
