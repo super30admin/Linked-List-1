a5e8f6d9ff317d801cb37449625ac9a89f650e8b
roysouravv30 <roysouravv30@gmail.com>
2021-10-14 22:26:39 -0400

    Linked List 1 updated

--- cycle.java
+++ cycle.java
@@ -8,18 +8,27 @@ public boolean hasCycle(ListNode head) {
 
     //The logic behind this approach is that if we move the fast pointer at a pace twice of that of
     // the slow pointer, if there exists a cycle, then the fast and the slow pointer
-    //will point to the same node at one point of the loop
-    if(head == null) return false;
-    ListNode dummy = new ListNode(0);
-    dummy.next = head;
-    ListNode fast = dummy;
-    ListNode slow = dummy;
-    
-    while(fast!=null && fast.next != null){
-        fast = fast.next.next;
-        slow = slow.next;
+    //will point to the same node at one point of the loop.
+    //When fast == slow, we start another slow_2 pointer until the slow and the slow2 pointer
+    //meet each other.
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
         
-        if(slow == fast) return true;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                ListNode slow_2 = dummy;
+                while(slow_2 != slow){
+                    slow = slow.next;
+                    slow_2 = slow_2.next;
+                }
+                return slow;
+            }
+        }
+        return null;
     }
-    return false;
-}
