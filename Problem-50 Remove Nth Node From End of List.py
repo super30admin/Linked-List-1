d27f52c894cd3498ff92bc2e7c67451d2fb536e6
Adhish Shrivastava <adhish70@gmail.com>
2021-10-29 15:14:33 -0400

    Problem50 done

--- Problem-50 Remove Nth Node From End of List.py
+++ Problem-50 Remove Nth Node From End of List.py
@@ -1,10 +1,12 @@
 # 19. Remove Nth Node From End of List
 # https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
-# Time Complexity:
-# Space Complexiety: 
+# Time Complexity: O(n)
+# Space Complexiety: O(1)
 
-# Logic: 
+# Logic: Use two pointers. The two pointers should be n distance apart. Then iterate the LL. 
+# When the second pointer reachs the last node, the first pointer will be n units behind. 
+# This is the location where we have to delete the node.
 
 
 # Definition for singly-linked list.
@@ -13,5 +15,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head, n: int):
-        pass
+    def removeNthFromEnd(self, head, n):
+        if head.next == None:
+            return head.next
+        
+        cur = head
+        end = head
+        
+        for i in range(n):
+            end = end.next
+        
+        if end == None:
+            head = head.next
+            return head
+        
+        while end.next:
+            cur = cur.next
+            end = end.next
+        
+        cur.next = cur.next.next
+        
+        return head
