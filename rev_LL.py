8f308bd2ad3de07b7000782b1a504d4a66ba9754
ManjariK0308 <83307484+ManjariK0308@users.noreply.github.com>
2021-10-28 19:31:07 -0700

    Completed LL1. Please Review

---
+++ rev_LL.py
@@ -0,0 +1,21 @@
+#TC = O(n)
+#SC = O(1)
+#Run on LC
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while (curr != None):
+            n = curr.next
+            curr.next = prev
+
+            prev = curr
+            curr = n
+        return prev
