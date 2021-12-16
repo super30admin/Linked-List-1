a16f4e44a8bbd13c254d9b13125335bbc090c231
Tejaswi Konduri <tejaswik5592@gmail.com>
2021-12-15 17:11:52 -0800

    Added code for all 3 problems

---
+++ removeNnodeFromEnd.java
@@ -0,0 +1,43 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// Logic: Take a dummy node and point it to head, so that you can delete the head and not cause a problem.
+// Have a fast and a slow pointers and increment the fast pointer till the given n value.
+// Move both pointers till fast pointer becomes null. and then skip the next pointer to the slow pointer.
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+        int count = 0;
+
+        while(count <= n) {
+            fast = fast.next;
+            count++;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
