0ebf49e31583e553842453dfb7e71a055d6ab64e
Dhruv Jani <dhruv.jani@rocketmail.com>
2021-12-31 15:31:41 -0500

    Completed Linked List 1

---
+++ RemoveNthElement.java
@@ -0,0 +1,23 @@
+public class RemoveNthElement {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode fast = dummyNode;
+        ListNode slow = dummyNode;
+
+        while(n >= 0) {
+            fast= fast.next;
+            n--;
+        }
+
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummyNode.next;
+    }
+}
---
+++ ReverseLinkedList.java
@@ -0,0 +1,22 @@
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fast = head.next;
+
+        while(fast != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;
+        }
+
+        curr.next = prev;
+
+        return curr;
+    }
+}
