87003e82d3b7185208edda05fadab5f797a2c3c9
Jatin Mehta <jatin010mehta@jatins-mbp.lan>
2022-01-11 17:16:51 -0500

    Please Review

---
+++ src/ReverseList.java
@@ -0,0 +1,22 @@
+//TC O(n)
+//SC O(1)
+public class ReverseList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
