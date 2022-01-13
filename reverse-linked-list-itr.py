be8d55bb0f6c6dce8f7735c26d14cae581450525
DEEPAKRAJ DHARMAPURI SELVAKUMAR <deepak.rajds2014@gmail.com>
2022-01-13 01:28:38 -0500

    LL I complete

---
+++ reverse-linked-list-itr.py
@@ -0,0 +1,26 @@
+'''
+TC: O(n)
+SC: O(1)
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        prev = head
+        curr = head.next
+        prev.next = None
+        
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        return prev
