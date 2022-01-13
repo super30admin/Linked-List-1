497b73b2db6476190b2ca1bdcce294ef9b2f9a34
TJayashree1412 <45650002+TJayashree1412@users.noreply.github.com>
2022-01-13 14:09:34 -0600

    Completed. Please review

--- ReverseLL.java
+++ ReverseLL.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n)
-// Space Complexity : O(1)
+// Space Complexity : O(n) uses stack n in recursion
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
