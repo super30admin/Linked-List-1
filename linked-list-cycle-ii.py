d9d368195ffeaa394435e7c230740622e6db94ca
Jayavardhan Karampudi <jay@Jais-MacBook-Pro.local>
2022-01-12 21:34:47 -0700

    solved the problems

--- linked-list-cycle-ii.py
+++ linked-list-cycle-ii.py
@@ -4,17 +4,35 @@
 #         self.val = x
 #         self.next = None
 # Time Complexity: O(n)
-# Space Complexity: O(n)
+# Space Complexity: O(1)
 class Solution(object):
     def detectCycle(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        visited = set()
-        temp = head
-        while temp is not None:
-            if temp in visited:
-                return temp
-            visited.add(temp)
-            temp = temp.next
+        #Optimized
+        if head == None: return head
+        slow = head
+        fast = head
+        flag = False
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                flag = True
+                break
+        if not flag: return None
+        fast = head
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+        return slow
+        #BruteForce
+        # visited = set()
+        # temp = head
+        # while temp is not None:
+        #     if temp in visited:
+        #         return temp
+        #     visited.add(temp)
+        #     temp = temp.next
