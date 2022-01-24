8bed9bedf14f700f42e902b8c85840b32cb4c3c3
Chirag Rajpal <chiragbrajpal@gmail.com>
2022-01-23 20:19:12 -0800

    Update Problem3LinkListCycle.java

--- Problem3LinkListCycle.java
+++ Problem3LinkListCycle.java
@@ -1,5 +1,7 @@
 
 
+
+
 //1st approach using hashSet using O(N) time;
 // and O(N) Space Complexity
 
@@ -14,5 +16,44 @@ public class Solution {
             curr=curr.next;
         }
         return curr;
+    }
+}
+
+
+//Efficient Way:
+//Taking two pointers
+//TC:O(N)
+//SC:O(1)
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        ListNode slowptr=head;
+        ListNode fastptr=head;
+        
+        //Need to check null as well, as test of null is included as well;
+        if(head==null){
+            return null;
+        }
+    
+        //need to check if there is a cycle or not as well  missing to check this thing;
+        boolean hasCycle=false;
+        while(fastptr!=null && fastptr.next!=null){     //also, fastptr!=null is to be checked firs then check fastptr.next if qual to null;
+            fastptr=fastptr.next.next;
+            slowptr=slowptr.next;
+            if(slowptr==fastptr){
+                hasCycle=true;
+                break;
+            }
+        }
+        
+        if(hasCycle==false){
+            return null;
+        }
+        slowptr=head;
+        while(slowptr!=fastptr){
+            slowptr=slowptr.next;
+            fastptr=fastptr.next;
+        }
+        return fastptr;
     }
 }
