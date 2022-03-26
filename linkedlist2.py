2ccee67b0e94a57dab46a4243f95de934dbb67dc
nisargushah <58926548+nisargushah@users.noreply.github.com>
2022-03-26 18:03:17 -0400

    Update linkedlist2.py

--- linkedlist2.py
+++ linkedlist2.py
@@ -4,6 +4,9 @@
 #         self.val = x
 #         self.next = None
 
+## Time Complexity O(n)
+## Space Complexity O(1)
+
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow, fast = head, head
