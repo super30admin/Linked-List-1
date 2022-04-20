59d1d6f90ba150e2caf82c40987edcb8f918d2f4
KalyaniJawanjal <jawanjal.kalyani06@gmail.com>
2022-04-20 16:09:00 -0700

    LinkedList-1-Please-review

---
+++ LinkedList-Cycle-II.java
@@ -0,0 +1,28 @@
+//time-O(n)
+//space-O(1)
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if(hasCycle==false) return null;
+        slow = head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+
+        return fast;
+    }
+}
