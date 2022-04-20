4a062e571d90dc584a4ebfe84346a9e94ebd6a25
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-04-19 23:07:23 -0700

    Add files via upload

---
+++ ReverseLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class ReverseLinkedList
+    {
+        /*
+         * T.C: O(N) : traversing through whole linked list
+         * S.C: O(1): no extra space used
+         */
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode prev = null;
+            ListNode current =head;
+            ListNode fast = head.next;
+
+            while(fast!=null)
+            {
+                current.next = prev;
+                prev = current;
+                current = fast;
+                fast = fast.next;
+            }
+
+            current.next = prev;
+
+            return current;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
