7379f81492f103a421066e1e1d745b5f37af2c4b
Jay Raj <jaypravinkumar.raj@calbaptist.edu>
2022-05-22 14:36:39 -0400

    Please Review: Linked-List-1

---
+++ RemovNodeFromNth.java
@@ -0,0 +1,38 @@
+public class RemovNodeFromNth {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        //null case
+
+        if(head == null){
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode fast =dummy;
+        int count=0;
+
+        while(count <=n){
+            fast = fast.next;
+            count++;
+
+        }
+
+        ListNode slow = dummy;
+        while(fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+
+    }
+
+}
---
+++ ReverseLinkedList.java
@@ -0,0 +1,34 @@
+public class ReverseLinkedList {
+
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null){
+            return null;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fast = head.next;
+
+
+        while(fast!=null){
+
+            curr.next=prev;
+            prev = curr;
+            curr =fast;
+            fast = fast.next;
+
+        }
+
+        curr.next = prev;
+        return curr;
+    }
+}
