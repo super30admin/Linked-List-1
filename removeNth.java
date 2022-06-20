5d6052f2a10b25bf6cdc1ad5c6a1b1c4b0e32a3c
Damandeep Singh <sdamandeep18@gmail.com>
2022-06-20 10:41:31 -0700

    Completed Linked list-1

---
+++ removeNth.java
@@ -0,0 +1,23 @@
+//time complexity: O(n)
+//space complexity: O(1)
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+      ListNode slow = head;
+      ListNode fast = head;
+  
+      while (n-- > 0)
+        fast = fast.next;
+      if (fast == null)
+        return head.next;
+  
+      while (fast.next != null) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      slow.next = slow.next.next;
+  
+      return head;
+    }
+  }
+  
+  
