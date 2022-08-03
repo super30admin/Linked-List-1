b1dda253931f43df5968725328aa4493bb4e02c7
harjotsbhatia <89289963+harjotsbhatia@users.noreply.github.com>
2022-08-02 18:52:06 -0700

    Create Linked List Cycle II.py

---
+++ Linked List Cycle II.py
@@ -0,0 +1,15 @@
+# Tc: O(n)
+# SC: O(n)
+class Solution(object):
+    def detectCycle(self, head):
+        visited = set()
+
+        node = head
+        while node is not None:
+            if node in visited:
+                return node
+            else:
+                visited.add(node)
+                node = node.next
+
+        return None
