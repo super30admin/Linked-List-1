c30532b31988b41c8f307e2c58d1fe0e67e1a24c
Charan Sundar Telaganeni <charansundartelaganeni@charansacstudio.mynetworksettings.com>
2022-09-22 19:28:54 -0400

    completed linked-list-1

---
+++ Remove nth node.py
@@ -0,0 +1,22 @@
+# TC : O(N)
+# SC : O(1)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        fast = slow = head
+        for i in range(n):
+            fast = fast.next
+        if not fast: return head.next
+        while fast.next:
+            fast, slow = fast.next, slow.next
+        slow.next = slow.next.next
+        return head
