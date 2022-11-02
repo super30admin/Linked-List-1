49e977fb25551a4b33d54b5fc42c1274bcc978ca
Kishore Kumar Kotagodakinda <kishorekumarkotagodakinda@KishorearaksAir.hitronhub.home>
2022-11-01 20:42:30 -0500

    Completed two questions

---
+++ LinkedList_cycle_2.py
@@ -0,0 +1,22 @@
+# TC: O(n)
+# SC: O(1)
+# Does this code run on Leetcode: yes
+# Did you face any difficulty while coding this: No
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast: break
+        else: return None  # if not (fast and fast.next): return None
+        while head != slow:
+            head, slow = head.next, slow.next
+        return head
+        
---
+++ Reverse_Linkedlist.py
@@ -0,0 +1,19 @@
+# TC: O(n)
+# SC: O(1)
+# Does this code run on Leetcode: Yes
+# Did you face any difficulties in coding this: No
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, current = None, head
+        while current:
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp
+        return prev
