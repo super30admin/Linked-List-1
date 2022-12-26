55e3637bba974ef9f0725c3e02837d9e0cc36590
sharanya-t <sharanyat@sharanyas-mbp.mynetworksettings.com>
2022-12-26 14:18:20 -0500

    Completed

---
+++ Problem#50.java
@@ -0,0 +1,30 @@
+// REVERSE LINKED LIST
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : -
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode reversed = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reversed;
+    }
+}
---
+++ Problem#52.java
@@ -0,0 +1,50 @@
+// LINKED LIST CYCLE II
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : -
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
+        }
+
+        if(!flag)
+            return null;
+        
+        //Start point of the cycle
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
