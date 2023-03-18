93f1442cc748844870c0cf20deb2e0e380395ef8
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-03-18 19:33:15 -0400

    Create reverseListRecursive.java

---
+++ reverseListRecursive.java
@@ -0,0 +1,13 @@
+//Recursive
+//TC: O(n)
+//SC: O(n)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+       
+        ListNode reversed = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reversed;
+}
+}
