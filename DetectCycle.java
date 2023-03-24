16a77e85fd9e01ba29a26e84b3a04e5e4c4681c2
Sriharsha2799 <spodichenapally@albany.edu>
2023-03-24 03:18:55 -0400

    Linked List-1 completed

---
+++ DetectCycle.java
@@ -0,0 +1,43 @@
+/* the time complexity of this implementation is O(n)
+ */
+public class DetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+        if (!hasCycle) {
+            return null;
+        }
+        ListNode ptr1 = head;
+        ListNode ptr2 = slow;
+        while (ptr1 != ptr2) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        return ptr1;
+    }
+    public static void main(String[] args) {
+        // create a linked list with a cycle
+        ListNode head = new ListNode(3);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(0);
+        ListNode node3 = new ListNode(-4);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node1;
+
+        // detect the cycle and print the node where it begins
+        ListNode cycleStart = detectCycle(head);
+        System.out.println("Cycle starts at node with value: " + cycleStart.val);
+    }
+
+}
---
+++ ReverseLinkedList.java
@@ -0,0 +1,44 @@
+/*the time complexity of this implementation is O(n)
+* */
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public static void main(String[] args) {
+        // create a linked list
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // print the original list
+        System.out.println("Original list:");
+        printList(head);
+
+        // reverse the list
+        ListNode reversed = reverseList(head);
+
+        // print the reversed list
+        System.out.println("Reversed list:");
+        printList(reversed);
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+}
