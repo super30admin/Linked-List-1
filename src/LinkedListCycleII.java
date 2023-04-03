52961b1eb38b246d56a01574d33a43ef66fdb103
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-04-03 09:57:39 -0700

    Create LinkedListCycleII.java

---
+++ src/LinkedListCycleII.java
@@ -0,0 +1,38 @@
+// Time Complexity:  O(n)
+// Space Complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public ListNode detectCycle(ListNode head) 
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+        while(!hasCycle && fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            hasCycle = (slow == fast);
+        }
+        if(!hasCycle)
+            return null;
+        ListNode p = head;
+        while(p!=slow)
+        {
+            p = p.next;
+            slow = slow.next;
+        }
+        return p;
+    }
+}
