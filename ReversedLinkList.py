edcdb3cf596ba5d86f7e921c962c76f2bd13559b
TrivediShaili <126596926+TrivediShaili@users.noreply.github.com>
2023-04-04 13:27:27 -0400

    Add files via upload

---
+++ ReversedLinkList.py
@@ -0,0 +1,17 @@
+# Time Complexity : O(N)
+# Space Complexity : O(1)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head:ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev
