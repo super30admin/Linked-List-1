11e3463e3d034ccc772fb5dfa81d42225c238f62
PRAYAG-PATEL-1998 <90693541+PRAYAG-PATEL-1998@users.noreply.github.com>
2023-06-01 17:52:47 -0700

    Update Sample.java
    Linked-List-1

--- Sample.java
+++ Sample.java
@@ -1,7 +1,59 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+class Solution {
+    ListNode reversed = null;
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)return head;
+        reversed = reverseList(head.next);
+        //st.pop() 4,3,2,1
+        head.next.next = head;
+        head.next = null;
+        return reversed;    
+    }
+}
 
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1,head);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        int count = 0;
+        while(count <= n){
+            fast = fast.next;
+            count++;
+        }
+        while(fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        //slow.next.next = null;
+        return dummy.next;
+        
+    }
+}
 
-// Your code here along with comments explaining your approach
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == false) return null;
+        slow = head;
+              
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    
+    }
+}
