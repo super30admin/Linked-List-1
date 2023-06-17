b9de183c68e2ccf2038fb4b4c0451ad2b6e92e8a
KISHORESHANKAR20062000 <60849170+KISHORESHANKAR20062000@users.noreply.github.com>
2023-06-17 00:53:59 -0700

    Completed reversing of Linked List

---
+++ reverseLinkedList.py
@@ -0,0 +1,31 @@
+#Time Complexity: O(N)
+#Space Complexity: O(N)
+#Did It run in Leetcode: Yes
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #This condition checks if the element present is single element or the linked list is empty
+        if(head==None or head.next==None):
+            return head
+        
+        #Variable assignment
+        prev=None
+        curr=head
+        fast=head.next
+
+        #This while loop and the following condition takes care of reversing the linked list and traverses through the Linked List
+        while(fast!=None):
+            curr.next=prev
+            prev=curr
+            curr=fast
+            fast=fast.next
+        #Assigning last element the previous value
+        curr.next=prev
+        return curr
