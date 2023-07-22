3f7d123e77f583b130cf2bd33cd4c54b429f912e
akshay <er.akshay@gmail.com>
2023-07-21 17:57:25 -0700

    LinkedList Cycles

---
+++ Solution.java
@@ -0,0 +1,89 @@
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution {
+
+    // Time Complexity : O(n)
+    // Space Complexity : O(1)
+    public ListNode reverseList_1(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = head;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    // Time Complexity : O(n)
+    // Space Complexity : O(1)
+    public ListNode reverseList_2(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = head;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+
+    // Time Complexity : O(n)
+    // Space Complexity : O(1)
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean isCycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                isCycle = true;
+                break;
+            }
+        }
+        return isCycle;
+    }
+
+    // Time Complexity : O(n)
+    // Space Complexity : O(1)
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean isCycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                isCycle = true;
+                break;
+            }
+        }
+        if (!isCycle) {
+            return null;
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
