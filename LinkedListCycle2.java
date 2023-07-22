9d961942dbd2315ce524660efad821811e38e0ac
somonnoy <sb7238@g.rit.edu>
2023-07-21 22:53:00 -0400

    Completed using Brute Force Approach

---
+++ LinkedListCycle2.java
@@ -0,0 +1,25 @@
+
+
+import java.util.HashSet;
+public class LinkedListCycle2 {
+    public ListNode detectCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return null; // No cycle in a linked list with 0 or 1 node
+        }
+
+        HashSet<ListNode> visitedNodes = new HashSet<>();
+
+        ListNode curr = head;
+        while (curr != null) {
+            if (visitedNodes.contains(curr)) {
+                return curr; // Found the node where the cycle begins
+            }
+            visitedNodes.add(curr);
+            curr = curr.next;
+        }
+
+        return null; // No cycle found
+
+    }
+}
