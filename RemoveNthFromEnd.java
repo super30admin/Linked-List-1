ecf987696a44f50a5f82230819cdd0f3de224570
sanahpathan <134433810+sanahpathan@users.noreply.github.com>
2023-08-25 15:22:26 -0700

    Create RemoveNthFromEnd.java

---
+++ RemoveNthFromEnd.java
@@ -0,0 +1,20 @@
+public class RemoveNthFromEnd {
+    //Time Complexity = O(1)
+    //Space Complexity = O(1)
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode fast=head.next;
+        while(fast!=null){
+            curr.next=prev;
+            prev=curr;
+            curr=fast;
+            fast=fast.next;
+        }
+        curr.next=prev;
+        return curr;
+    }
+}
