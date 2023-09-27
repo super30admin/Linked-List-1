477a931c6bfc6a5b702905dc7cd2066062724906
sshreya99 <143497505+sshreya99@users.noreply.github.com>
2023-09-26 19:56:17 -0700

    Create reverse-linked-list_LC_206.py

---
+++ reverse-linked-list_LC_206.py
@@ -0,0 +1,26 @@
+#Three pointers TC: O(N) SC: (1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        prev=None
+        curr=head
+        fast=head.next
+
+        while(fast!=None):
+            curr.next=prev
+            prev=curr
+            curr=fast
+            fast=fast.next
+
+        curr.next=prev   
+        
+        return curr
