b5f777c3d21df27a4e513b4c5512b7f43e06d1cd
UmangThakur15 <umangthakuremail@gmail.com>
2023-10-03 16:23:17 -0400

    linkedListCycle

--- linkedListCycle.java
+++ linkedListCycle.java
@@ -45,6 +45,33 @@ public class linkedListCycle {
         return slow;
 
     }
+
+    public static void main(String[] args){
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        // Create a cycle by connecting the last node to the second node
+        node5.next = node2;
+
+        // Create an instance of the linkedListCycle class
+        linkedListCycle solution = new linkedListCycle();
+
+        // Detect and print the start of the cycle (if it exists)
+        ListNode cycleStart = solution.detectCycle(head);
+
+        if (cycleStart != null) {
+            System.out.println("Cycle starts at node with value: " + cycleStart.val);
+        } else {
+            System.out.println("No cycle detected in the linked list.");
+        }
+    }
 }
 
 
