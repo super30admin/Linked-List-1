481dcd7d3742072a56d3d61b9edeb231ed264b6d
UmangThakur15 <umangthakuremail@gmail.com>
2023-10-03 16:15:57 -0400

    linkedListCycle

---
+++ linkedListCycle.java
@@ -0,0 +1,50 @@
+
+//TC will be O(n)
+//SC will be O(1)
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+public class linkedListCycle {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if(!hasCycle){
+            return null;
+        }
+
+        fast = head;
+
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+
+    }
+}
+
+
