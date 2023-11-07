3797f73b1194295b4573aee2a2d69778fc6ef12d
Aditya Kondepudi <36687530+adityakondepudi@users.noreply.github.com>
2023-11-07 17:35:47 -0500

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
