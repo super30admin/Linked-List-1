030abd4f42cd99356cadd3b151cc2def6f3d4111
Mridang Kejriwal <mridangkejriwal@Mridangs-MacBook-Air.local>
2023-12-01 10:24:10 -0800

    Done Linked List 1

---
+++ linkedlistcycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if slow == fast:
+                while slow!=head:
+                    slow = slow.next
+                    head = head.next
+                return slow
+
+
+        return None
+
---
+++ reverse.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        curr=head.next
+        prev=head
+        prev.next=None
+        while curr:
+            temp1=curr.next
+            curr.next=prev
+            prev=curr
+            curr=temp1
+        
+        return prev
+        
