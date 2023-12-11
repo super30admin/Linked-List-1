5db4f4461cfef3b4091c9bdfff03ab88171710b2
sirisha pydimarri <sirishapydimarri23@gmail.com>
2023-12-11 18:13:08 -0400

    Completed

--- LLCycle2.java
+++ LLCycle2.java
@@ -1,7 +1,41 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n) // 2 times O(n) 1 cycle for checking for cyclic dependency, another to find the cycle node
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this :
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+//Using 2 pointer approach to find if there is cyclic dependency, if yes, then move slow to head and move slow and
+// fast at 1x speed until they meet.
+public class LLCycle2 {
+    public ListNode detectCycle(ListNode head) {
 
-// Your code here along with comments explaining your approach
+        //Check if there is a circular dependency or not
+        boolean  flag = false;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag) return null;
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;//in circular, they meet with 1x speed
+        }
+        return fast;
+    }
+}
--- RemoveNthNode.java
+++ RemoveNthNode.java
@@ -1,7 +1,46 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this :
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 
-// Your code here along with comments explaining your approach
+//We are using 2 pointer approach, first pointer will be n steps ahead of second.Both pointers move simultaneously
+// until first reaches end node.  Then the second will be at the node-1 that needs to be removed.Use dummy node to
+//handle edge cases
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1); //dummy node is needed for edge case when we need to remove last element.
+        dummy.next = head;
+
+        ListNode slow = dummy, fast = dummy;
+        if(head.next==null) {
+            return null;
+        }
+        int count = 0; //starting at 0 coz we want fast ptr 1 step ahead of difference n
+        while(count <= n){
+            fast = fast.next;
+            count++;
+        }
+        while(fast!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Use a temp node to explicitly nullify the removed node
+        ListNode temp = slow.next;
+        slow.next = slow.next.next;
+        temp.next = null; //this will facilitate gc
+
+        return dummy.next;
+    }
+}
