e4d5e1d8e911c4b74c7a58b330faa8e89b73dc00
Anudeep Uppu <Anudeep.Uppu@nau.edu>
2024-01-04 01:11:54 -0700

    Completed

---
+++ ReverseLinkedList.java
@@ -0,0 +1,21 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next;
+
+        while(curr != null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
