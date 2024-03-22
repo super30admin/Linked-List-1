4b659759d70105d9cccc7f9bf8de89975d8038a2
ParvathiRPai <parvathirpai@outlook.com>
2024-03-22 15:57:05 -0700

    Linked List -1

---
+++ problem1.py
@@ -0,0 +1,23 @@
+# Time complexity is O(1) and space complexity is O(1)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        prev = None
+        curr = head
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            
+        return prev
